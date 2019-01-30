922beb27b6da1d9c31795bb2b5c2a0ebe7fb8042
HassanSaeed92626 <43633069+HassanSaeed92626@users.noreply.github.com>
2019-01-30 16:52:10 +0500

    updated home.php

--- home.php
+++ home.php
@@ -58,3 +58,154 @@
 		</div>	
 
 	</div>
+
+</nav>
+
+
+
+<section id ="Home" class="header-home" >
+	
+<div class = "container">
+
+	<div class = "row">
+		<div class = "col-sm-7 col-header-text lr-padding ">
+			<h2> Welcome to Asman.com</h2>
+			
+			<p> Asman.com is the first and largest property portal in Pakistan and is among the top five property portals in the world. It was founded in 2006 and has since revolutionised the real estate industry of Pakistan by connecting buyers and sellers online in a highly convenient way, making it a household name among Pakistanis around the world. With over 350,000 new listings each month, and over 5 million monthly users, it is the pioneering property portal of Pakistan, with more than 12,500 agencies registered.		 
+			</p>
+
+			<a href = "login.php" class = "banner_btn btn_white"> Log in </a>
+			<a href = "signup.php" class = "banner_btn btn-transparent "> Sign up </a>		
+		</div>
+	</div>
+
+</div> 
+</section>
+
+
+
+ <div id="contenarea">
+            <div id="side">
+         <div class="thumbs"> <!--thumb 1-->
+             <h2> 3 Marla </h2>
+             <img src="3-marla.jpg" style="float:left;width:100%;height:100%;">
+         </div>
+
+         <div class="thumbs"> <!--thumb 2-->
+            <h2> 5 Marla </h2>
+             <img src="5-marla.jpg" style="float:left;width:100%;height:100%;">
+         </div>
+
+         <div class="thumbs"> <!--thumb 3-->
+            <h2> 7 Marla </h2>
+             <img src="3-marla.jpg" style="float:left;width:100%;height:100%;">
+         </div>
+
+         <div class="thumbs">  <!--thumb 4-->
+             <h2> 1 Kanal </h2>
+             <img src="1-kanal.jpg" style="float:left;width:100%;height:100%;">
+         </div>
+
+         <div class="thumbs"> <!--thumb 5-->
+            <h2> 2 Kanal  </h2>
+             <img src="3-kanal.jpg" style="float:left;width:100%;height:90%;">
+         </div>
+
+
+         <div class="thumbs"> <!--thumb 6-->
+            <h2> 3 Kanal  </h2>
+             <img src="1-kanal.jpg"  style="float:left;width:100%;height:100%;">
+         </div>
+     </div> <!--end of side-->
+
+
+     </div> <!--end of content area-->
+
+
+
+
+
+<footer class="section footer-classic context-dark bg-image" style="background: #2d3246;">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4">
+                <p>Asman.com is the first and largest property portal in Pakistan and is among the top five property portals in the world. It was founded in 2006 and has since revolutionised the real estate industry of Pakistan by connecting buyers and sellers online in a highly convenient way, making it a household name among Pakistanis around the world. With over 350,000 new listings each month, and over 5 million monthly users, it is the pioneering property portal of Pakistan, with more than 12,500 agencies registered.</p>
+          
+                <p class="rights">All Rights Reserved To Asman.com.</p>
+              </div>
+            </div>
+           
+
+ <div class="col-md-4 col-xl-3"></div>
+
+            <div class="col-md-4 col-xl-3">
+              <h5>Links</h5>
+              <ul class="nav-list">
+
+			<li><a href="">Home</a></li>
+			<li><a href="">Log-in</a></li>
+			<li><a href="">Register</a></li>
+			<li><a href="">Forget Password?</a></li>
+			<li><a href="">Contact</a></li>
+			<li><a href="">About</a></li>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+</footer>
+
+</body>
+
+
+
+
+
+<script>
+  	var slider_content = document.getElementById('Home');
+
+  	// contain images in an array
+    var image = ['1','2','3'];
+    var i = 1;
+    
+
+    // function for next slide 
+
+    function nextImage(){
+    	if (i<image.length)
+    	{
+    		slider_content.style.backgroundImage = "url("+image[i]+".gif)";
+    		i++;
+    		//alert("IF "+ i);
+    	}
+        else if(i===image.length)
+    	{
+    		i=0;
+    		//alert("else "+ i);
+    	}
+        
+    }
+    // function for prew slide
+
+    function prewImage()
+    {
+
+        if (i<image.length+1 && i>1)
+        {
+            i= i-1;
+        }
+        else
+        {
+            i = image.length;
+        }
+        slider_content.style.backgroundImage = "url("+image[i-1]+".gif)";
+    }
+    if(i<image.length)
+    	setInterval(nextImage , 4000);
+    else{
+    	i=1;
+    	setInterval(prewImage , 4000);
+    }
+</script>
+</html>
